f8ae1b29453dcdd9d3cd09b8840709e19b9ba252
aliscafo <ero.alina@gmail.com>
2016-12-21 23:22:25 +0300

    upd

--- HomeTask11/test_model.py
+++ HomeTask11/test_model.py
@@ -13,6 +13,14 @@ class TestNumber(unittest.TestCase):
             Print(abc["a"]).evaluate(abc)    
             self.assertEqual(out_number.getvalue(), str(100) + '\n')
 
+class TestScope(unittest.TestCase):
+    def test_scope(self):
+        scope = Scope()
+        scope["num"] = Number(505)
+        with patch("sys.stdout", new_callable=StringIO) as mock_out:
+            Print(scope["num"]).evaluate(scope)    
+            self.assertEqual(mock_out.getvalue(), str(505) + '\n')
+
 
 if __name__ == '__main__':
     unittest.main()       
